83a0598771429195a7955fe884ae9c096bce2db5
Antonio <despliegue@daw2.es>
2022-11-09 09:44:12 +0100

    PRIMER COMMIT

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Inicio Sesión</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    <form name="inicioSesión" method="POST" action="IncioSesion.php">
+        <input type="text" name="nombre">
+        <input type="password" name="contrasena">
+        <input type="submit" name="enviar" value="ENVIAR">
+    </form>
+</body>
+</html>
